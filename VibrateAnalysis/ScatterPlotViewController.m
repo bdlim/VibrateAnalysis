fdc71fe23a77a2abc8cb51750cc63c898413a21b
Brandon Lim <bdlim2004@gmail.com>
2014-11-17 23:22:52 -0800

    Revert "Revert 405b208..a9ed67f"
    This reverts commit d13eeb585fa22de0dcdee8b8ad567d2ab7e411b0.

--- VibrateAnalysis/ScatterPlotViewController.m
+++ VibrateAnalysis/ScatterPlotViewController.m
@@ -8,117 +8,16 @@
 
 #import "ScatterPlotViewController.h"
 
-
 @interface ScatterPlotViewController ()
 
 @end
 
 @implementation ScatterPlotViewController
 
-float *magnitude;
-float *phase_lev;
-NSInteger count;
-NSInteger seconds;
-NSTimer *timer;
-
 @synthesize hostView = hostView_;
-@synthesize managedObjectContext;
-
-- (IBAction)startRecording:(id)sender {
-	// Do any additional setup after loading the view, typically from a nib.
-    currentMaxAccelX = 0;
-    currentMaxAccelY = 0;
-    currentMaxAccelZ = 0;
-    
-    currentMaxRotX = 0;
-    currentMaxRotY = 0;
-    currentMaxRotZ = 0;
-    
-    self.motionManager = [[CMMotionManager alloc] init];
-    self.motionManager.accelerometerUpdateInterval = .2;
-    self.motionManager.gyroUpdateInterval = .2;
-    
-    self.dataQueue = [[NSMutableArray alloc] init];
-    self.accelx = [[NSMutableArray alloc] init];
-    self.accely = [[NSMutableArray alloc] init];
-    self.accelz = [[NSMutableArray alloc] init];
-    
-    seconds = 12;
-    //self.countLabel.text = [NSString stringWithFormat:@"%i", seconds];
-    [self.oneClickLabel setTitle:[NSString stringWithFormat:@"%i", seconds] forState:UIControlStateNormal];
-    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
-                                             target:self
-                                           selector:@selector(subtractTime) userInfo:Nil repeats:YES];
-    
-    [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
-                                             withHandler:^(CMAccelerometerData  *accelerometerData, NSError *error) {
-                                                 [self outputAccelerationData:accelerometerData.acceleration];
-                                                 if(error){
-                                                     
-                                                     NSLog(@"%@", error);
-                                                 }
-                                             }];
-    
-    [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue]
-                                    withHandler:^(CMGyroData *gyroData, NSError *error) {
-                                        [self outputRotationData:gyroData.rotationRate];
-                                    }];
-    
-    
-}
-
-- (IBAction)AnalysisButton:(id)sender {
-     [self performSegueWithIdentifier:@"MPAnalysis" sender:nil];
-}
-/*
-- (IBAction)resetMaxValues:(id)sender
-{
-    
-    currentMaxAccelX = 0;
-    currentMaxAccelY = 0;
-    currentMaxAccelZ = 0;
-    
-    currentMaxRotX = 0;
-    currentMaxRotY = 0;
-    currentMaxRotZ = 0;
-    
-    [self.dataQueue removeAllObjects];
-}
-*/
--(void) outputAccelerationData:(CMAcceleration)acceleration
-{
-    DataReadPoint *drp = [[DataReadPoint alloc] init];
-    drp.AccelX = acceleration.x;
-    drp.AccelY = acceleration.y;
-    drp.AccelZ = acceleration.z;
-    NSDate *date = [NSDate date];
-    
-    //NSLog(@"accel in xyz is %lf, %lf,%lf", drp.AccelX, drp.AccelY, drp.AccelZ);
-    
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    dateFormatter.dateFormat = @"hh:mm:ss";
-    [dateFormatter setTimeZone:[NSTimeZone systemTimeZone]];
-    drp.TimeStamp = [dateFormatter stringFromDate:date];
-    
-    [self.dataQueue enqueue:drp];
-    [self.accelx enqueue:[NSNumber numberWithDouble:acceleration.x]];
-    [self.accely enqueue:[NSNumber numberWithDouble:acceleration.y]];
-    [self.accelz enqueue:[NSNumber numberWithDouble:acceleration.z]];
-    [self.hostView.hostedGraph reloadData];
-    
-    float newX = [self.dataQueue count];
-    
-    if(seconds != 0)
-    {
-    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)self.hostView.hostedGraph.defaultPlotSpace;
-    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(newX-8.0) length:CPTDecimalFromFloat(16.0)];
-    }
-}
-
 
 - (void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    [self.oneClickLabel setTitle:[NSString stringWithFormat:@"Start Recording"] forState:UIControlStateNormal];
     [self initPlot];
 }
 
@@ -131,35 +30,35 @@ NSTimer *timer;
 
 - (void) configureHost {
     
-    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.graphView.bounds];
+    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.view.bounds];
     self.hostView.allowPinchScaling = YES; //FOR ZOOMING IN/OUT
-    [self.graphView addSubview:self.hostView];
+    [self.view addSubview:self.hostView];
 }
 
 - (void) configureGraph {
     
     //Create the graph
     CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
-    [graph applyTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme]];
+    [graph applyTheme:[CPTTheme themeNamed:kCPTSlateTheme]];
     self.hostView.hostedGraph = graph;
     
     //Set graph title
-    NSString *title = @"X-Y-Z Acceleration | Time";
+    NSString *title = @"Graph XYZ POSITION";
     graph.title = title;
     
     //Create and set text style
     CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
-    titleStyle.color = [CPTColor colorWithComponentRed:128 green:0 blue:0 alpha:1];
+    titleStyle.color = [CPTColor whiteColor];
     titleStyle.fontName = @"Helvetica-Bold";
     titleStyle.fontSize = 16.0f;
     graph.titleTextStyle = titleStyle;
     graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
-    graph.titleDisplacement = CGPointMake(0.0f, 22.0f);
+    graph.titleDisplacement = CGPointMake(0.0f, 10.0f);
     
     //Set padding for plot area
     [graph.plotAreaFrame setPaddingLeft:30.0f];
-    [graph.plotAreaFrame setPaddingBottom:10.0f];
-    [graph.plotAreaFrame setPaddingTop:30.0f];
+    [graph.plotAreaFrame setPaddingBottom:30.0f];
+    [graph.plotAreaFrame setPaddingTop:50.0f];
     
     //Enable user interactions for plot space
     CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
@@ -196,10 +95,8 @@ NSTimer *timer;
     [graph addPlot:zPlot toPlotSpace:plotSpace];
     
     // 3 - Set up plot space
-    float current_count = [self.dataQueue count];
-    NSLog(@"the current count is %f", current_count);
-    [plotSpace setYRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( -2.0 ) length:CPTDecimalFromFloat( 4.0 )]];
-    [plotSpace setXRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( 0 ) length:CPTDecimalFromFloat( 16 )]];
+    [plotSpace setYRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( -1.5 ) length:CPTDecimalFromFloat( 3.0 )]];
+    [plotSpace setXRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( 0 ) length:CPTDecimalFromFloat( 10 )]];
     
     // 4 - Create styles and symbols
     CPTMutableLineStyle *xLineStyle = [xPlot.dataLineStyle mutableCopy];
@@ -237,6 +134,7 @@ NSTimer *timer;
     zSymbol.lineStyle = zSymbolLineStyle;
     zSymbol.size = CGSizeMake(6.0f, 6.0f);
     zPlot.plotSymbol = zSymbol;
+    
 }
 
 - (void) configureAxes {
@@ -265,24 +163,14 @@ NSTimer *timer;
     CPTMutableLineStyle *gridLineStyle = [CPTMutableLineStyle lineStyle];
     tickLineStyle.lineColor = [CPTColor blackColor];
     tickLineStyle.lineWidth = 1.0f;
-    
+    // 2 - Get axis set
     CPTXYAxisSet *axisSet = (CPTXYAxisSet *) self.hostView.hostedGraph.axisSet;
-    
-    CPTMutableTextStyle *axisTitleStyle = [CPTMutableTextStyle textStyle];
-    axisTitleStyle.color = [CPTColor colorWithComponentRed:128 green:0 blue:0 alpha:1];
-    axisTitleStyle.fontName = @"Helvetica-Bold";
-    axisTitleStyle.fontSize = 12.0f;
-    CPTMutableLineStyle *axisLineStyle = [CPTMutableLineStyle lineStyle];
-    axisLineStyle.lineWidth = 2.0f;
-    axisLineStyle.lineColor = [CPTColor darkGrayColor];
+    // 3 - Configure x-axis
     CPTAxis *x = axisSet.xAxis;
-    x.title = @"Time";
+    x.title = @"Day of Month";
     x.titleTextStyle = axisTitleStyle;
     x.titleOffset = 15.0f;
     x.axisLineStyle = axisLineStyle;
-    //float current_count = [self.dataQueue count];
-    //x.visibleRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat((NSInteger)current_count) length:CPTDecimalFromFloat((NSInteger)5.0)];
-    /*
     x.labelingPolicy = CPTAxisLabelingPolicyNone;
     x.labelTextStyle = axisTextStyle;
     x.majorTickLineStyle = axisLineStyle;
@@ -376,17 +264,12 @@ NSTimer *timer;
                 updatedRange = mutableRange;
             }
             else {
-               /*
-                float current_count = [self.dataQueue count];
-                updatedRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(current_count) length:CPTDecimalFromFloat(3.0)];
-                NSLog(@"the current count is %f", current_count);
-                */
                 updatedRange = newRange;
             }
             break;
         case CPTCoordinateY:
             //SO Y AXIS Doesn't move
-            updatedRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( -2.0 ) length:CPTDecimalFromFloat( 4.0 )];//((CPTXYPlotSpace *)space).yRange;
+            updatedRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( -1.5 ) length:CPTDecimalFromFloat( 3.0 )];//((CPTXYPlotSpace *)space).yRange;
             break;
     }
     return updatedRange;
@@ -401,38 +284,10 @@ NSTimer *timer;
     return self;
 }
 
--(void)subtractTime{
-    seconds--;
-    
-    //self.countLabel.text = [NSString stringWithFormat:@"%i", seconds];
-    [self.oneClickLabel setTitle:[NSString stringWithFormat:@"%i", seconds] forState:UIControlStateNormal];
-    if(seconds == 0)
-    {
-        [timer invalidate];
-        NSDate *date = [NSDate date];
-        
-        
-        CCAppDelegate *appDelegate = (CCAppDelegate *)[[UIApplication sharedApplication]delegate];
-        self.managedObjectContext = [appDelegate managedObjectContext];
-        
-        NSManagedObject *dataRecord = [NSEntityDescription
-                                           insertNewObjectForEntityForName:@"DataRecord"
-                                           inManagedObjectContext:self.managedObjectContext];
-        NSData *xData = [NSKeyedArchiver archivedDataWithRootObject:self.accelx];
-        NSData *yData = [NSKeyedArchiver archivedDataWithRootObject:self.accely];
-        NSData *zData = [NSKeyedArchiver archivedDataWithRootObject:self.accelz];
-        [dataRecord setValue:xData forKey:@"accelX"];
-        [dataRecord setValue:yData forKey:@"accelY"];
-        [dataRecord setValue:zData forKey:@"accelZ"];
-        [dataRecord setValue:date forKey:@"date"];
-        NSError *error;
-        if (![self.managedObjectContext save:&error]) {
-            NSLog(@"Whoops, couldn't save: %@", [error localizedDescription]);
-        }
-        [self.motionManager stopAccelerometerUpdates];
-        
-        [self performSegueWithIdentifier:@"MPAnalysis" sender:nil];
-    }
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
@@ -451,6 +306,7 @@ NSTimer *timer;
 }
 
 - (NSNumber *) numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index{
+
     if(fieldEnum == CPTScatterPlotFieldX)
     {
         return [NSNumber numberWithInt: index];
@@ -461,57 +317,10 @@ NSTimer *timer;
             return [NSNumber numberWithDouble:[[self.dataQueue objectAtIndex:index] AccelY]];
         else if([plot.identifier isEqual:@"zPlot"] == YES)
             return [NSNumber numberWithDouble:[[self.dataQueue objectAtIndex:index] AccelZ]];
-
     }
     
 
     return [NSDecimalNumber zero];
-}
-
-- (IBAction)MPAnalysis:(id)sender {
-    NSLog(@"analysis chosed");
-    [self performSegueWithIdentifier:@"MPAnalysis" sender:nil];
-}
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    
-    if ([[segue identifier] isEqualToString:@"MPAnalysis"])
-    {
-         NSLog(@"about to segue");
-         AAnalysisController *AAC = [segue destinationViewController];
-        
-        [AAC setDataQueue:_dataQueue];
-    }
-    
-}
-
--(void) outputRotationData:(CMRotationRate)rotation
-{
-    /*
-     self.rotX.text = [NSString stringWithFormat:@" %.2fr/s",rotation.x];
-     if(fabs(rotation.x) > fabs(currentMaxRotX))
-     {
-     currentMaxRotX = rotation.x;
-     }
-     self.rotY.text = [NSString stringWithFormat:@" %.2fr/s",rotation.y];
-     if(fabs(rotation.y) > fabs(currentMaxRotY))
-     {
-     currentMaxRotY = rotation.y;
-     }
-     self.rotZ.text = [NSString stringWithFormat:@" %.2fr/s",rotation.z];
-     if(fabs(rotation.z) > fabs(currentMaxRotZ))
-     {
-     currentMaxRotZ = rotation.z;
-     }
-     
-     self.maxRotX.text = [NSString stringWithFormat:@" %.2f",currentMaxRotX];
-     self.maxRotY.text = [NSString stringWithFormat:@" %.2f",currentMaxRotY];
-     self.maxRotZ.text = [NSString stringWithFormat:@" %.2f",currentMaxRotZ];
-     */
 }
 
 @end
